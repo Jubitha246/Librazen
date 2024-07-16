9ed7ba6dba3fe8e7b2435c1b4159686436c6ebc0
Jubitha246 <jubithasri246@gmail.com>
2024-07-16 21:58:36 +0530

    Update book.model.js

--- backend/model/book.model.js
+++ backend/model/book.model.js
@@ -1,32 +1,23 @@
-import mongoose from "mongoose";
+
+import mongoose from 'mongoose';
+
+const borrowedBySchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  borrowDate: { type: Date, required: true },
+  dueDate: { type: Date, required: true },
+  returnedDate: { type: Date },
+  status: { type: String, enum: ['Pending', 'Returned'], default: 'Pending' }
+});
 
 const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-   },
-   issuedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  availableCopies:{type:Number,default:1},
-  borrowedBy:[{
-    user:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
-    dueDate:{type:Date,required:true}
-  }]
-});
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String },
+  availableCopies: { type: Number, default: 1 },
+  borrowedBy: [borrowedBySchema]
+}, { timestamps: true });
+
 const Book = mongoose.model('Book', bookSchema);
 
 export default Book;
