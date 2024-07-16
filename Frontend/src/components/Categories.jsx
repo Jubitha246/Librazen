d328b11a3a1415b26b19b87584a8f91d43099c61
Jubitha246 <jubithasri246@gmail.com>
2024-07-16 22:04:55 +0530

    Update Categories.jsx

--- Frontend/src/components/Categories.jsx
+++ Frontend/src/components/Categories.jsx
@@ -1,26 +1,29 @@
-import React ,{ useEffect, useState } from 'react';
+import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from './Cards';
-import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 function Categories() {
-  const [book,setBook] = useState([]);
-  useEffect(()=>{
-    const getBook = async() =>{
-      try{
-      const res = await axios.get("http://localhost:4001/book");
-      console.log(res.data);
-      setBook(res.data);
-    }catch(error){
-       console.log(error);
-    }};
-    getBook();
-  },[])
+  const navigate = useNavigate();
+
+  const categories = [
+    { name: 'Popular Books', image: '/images/Popular1.jpg' },
+    { name: 'New Arrivals', image: '/images/Popular1.jpg' },
+    { name: 'Genres', image: '/images/Popular1.jpg' },
+    { name: 'Recommended Reads', image: '/images/Popular1.jpg' },
+    { name: 'E-books', image: '/images/Popular1.jpg' },
+    { name: 'Children\'s Books', image: '/images/Popular1.jpg' },
+  ];
+
+  const handleViewClick = () => {
+    navigate('/signup');
+  };
+
   var settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
@@ -59,9 +62,9 @@ function Categories() {
         <h1 className="font-semibold text-xl pb-2">Categories</h1>
       </div>
       <Slider {...settings}>
-        {book.slice(0, 6).map(category => (
-          <div key={category.id} className="p-2">
-            <Cards category={category} />
+        {categories.map((category, index) => (
+          <div key={index} className="p-2">
+            <Cards category={category} handleViewClick={handleViewClick} />
           </div>
         ))}
       </Slider>
@@ -70,3 +73,4 @@ function Categories() {
 }
 
 export default Categories;
+
