42bde8aea4f0859d4ab233bb97fbf2fdaf95b20b
Jubitha246 <jubithasri246@gmail.com>
2024-07-16 22:03:44 +0530

    Update Cards.jsx

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
-function Cards({ category }) {
+function Cards({ category, handleViewClick }) {
   return (
     <div className="card bg-white w-92 shadow-xl hover:scale-105 duration-200">
       <figure>
         <img
           src={category.image}
           alt={category.name}
+          className="w-full h-50 object-cover"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{category.name}</h2>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary hover:bg-blue-400 hover:text-white duration-200 ">View</button>
+          <button 
+            className="btn btn-primary hover:bg-blue-400 hover:text-white duration-200"
+            onClick={handleViewClick}
+          >
+            View
+          </button>
         </div>
       </div>
     </div>
